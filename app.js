bd2903da85fff1fd43a016a0c0f3b7feac1f19ba
TheJolt3 <brunoax05@gmail.com>
2025-12-29 07:37:49 -0300

    commit
    aprendendo dia da semana
    criando sozinho

---
+++ app.js
@@ -0,0 +1,9 @@
+// Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+alert ('Qual o dia da semana?');
+let diaSemana = 'sábado ou domingo'; 
+let chute = prompt('Digite o dia da semana:');
+if (chute == diaSemana) {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
